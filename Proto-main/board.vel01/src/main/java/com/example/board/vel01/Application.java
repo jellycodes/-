72d3716ce4971c871b1ece76398637f9bd75e000
Bit-Moonoe <moonoe0815@gmail.com>
2022-12-09 18:53:48 +0900

    아띠 백엔드. 업데이트는 진행중

---
+++ Proto-main/board.vel01/src/main/java/com/example/board/vel01/Application.java
@@ -0,0 +1,17 @@
+package com.example.board.vel01;
+
+import com.example.board.vel01.base.jwt.JwtProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+
+@EnableConfigurationProperties({JwtProperties.class})
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
