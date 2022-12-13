b89a46cc03ef177af902a082ba54e65a6a747a83
jellycodes <jylee7942@gmail.com>
2022-12-14 04:30:46 +0900

    최종

---
+++ atti_frontend/next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+  }
+  
+  module.exports = {
+	  async rewrites() {
+		  return [
+			  {
+				  source: "/:path*",
+				  destination: "http://localhost:3000/:path*",
+			  },
+		  ];
+	  }
+  }
---
+++ atti_frontend/pages/index.js
@@ -0,0 +1,19 @@
+// import Head from 'next/head'
+// import Image from 'next/image'
+import Main from '../components/Layout/Main'
+// import styles from '../styles/Home.module.css'
+import 'semantic-ui-css/semantic.min.css'
+
+export default function Home() {
+  return (
+    <>
+      {/* 여기 말고 app.js에 내비바 넣어주면 페이지가 바껴도 계속 보인다. */}
+      {/* 주석은 ctrl+/ */}
+
+      <Main />
+      {/* 메인 부분 */}
+
+      
+    </>
+  )
+}
